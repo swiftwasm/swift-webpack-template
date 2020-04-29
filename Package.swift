6c4933f5947c852e5eee0c84c225e8e272b4bbdf
Yuta Saito <kateinoigakukun@gmail.com>
2020-04-29 21:28:32 +0900

    Update libraries

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
             targets: ["__PROJECT_NAME__"]),
     ],
     dependencies: [
-      .package(name: "JavaScriptKit", url: "https://github.com/kateinoigakukun/JavaScriptKit.git", .branch("master")),
+      .package(name: "JavaScriptKit", url: "https://github.com/kateinoigakukun/JavaScriptKit.git", .revision("49bd63a")),
     ],
     targets: [
         .target(
